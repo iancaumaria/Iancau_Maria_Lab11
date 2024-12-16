43025fcb582c06e61f686e9a67c7276691b3fa47
iancaumaria <iancau.maria.imf@gmail.com>
2024-12-16 18:20:30 +0200

    Add project files.

---
+++ IancauMaria_Lab11/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using IancauMaria_Lab11.Data;
+namespace IancauMaria_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
